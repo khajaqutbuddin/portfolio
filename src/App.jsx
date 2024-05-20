eb76c7cb446be0d3914722e458f0a34318c4857a
Khaja Qutbuddin <khajaqutbuddin123@gmail.com>
2024-05-21 01:15:11 +0530

    Save local changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className=' font-extralight font-bold'>
-      <h1>Hello thsi is a Khaja Full Stack developer</h1>
+     <div className=' font-mono text-center text-gray-900 font-bold'>
+      <h1>Hello this is a Khaja Full Stack developer</h1>
+
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad aut necessitatibus labore.</p>
      </div>
     </>
