30dd477ac8e230fd57285d4c6eecc6ee16cbec1a
Khaja Qutbuddin <khajaqutbuddin123@gmail.com>
2024-05-21 00:53:32 +0530

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className=' font-extralight font-bold'>
+      <h1>Hello thsi is a Khaja Full Stack developer</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad aut necessitatibus labore.</p>
+     </div>
+    </>
+  )
+}
+
+export default App
